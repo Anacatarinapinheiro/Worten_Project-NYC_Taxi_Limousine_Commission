50d38bcda35dd61e3472149e56fe911686a6d179
Ana Catarina Pinheiro <157593461+Anacatarinapinheiro@users.noreply.github.com>
2024-09-03 13:23:26 +0100

    Update convert_Parquet.py

--- convert_Parquet.py
+++ convert_Parquet.py
@@ -1,33 +1,33 @@
 import pandas as pd
 
-# #converter yellow cabs
-#df = pd.read_parquet('yellow_tripdata_2015-01.parquet')
-#df.to_csv('yellow_tripdata_2015-01.csv')
+#converter yellow cabs
+df = pd.read_parquet('yellow_tripdata_2015-01.parquet')
+df.to_csv('yellow_tripdata_2015-01.csv')
 
-#df = pd.read_parquet('yellow_tripdata_2016-01.parquet')
-#df.to_csv('yellow_tripdata_2016-01.csv')
+df = pd.read_parquet('yellow_tripdata_2016-01.parquet')
+df.to_csv('yellow_tripdata_2016-01.csv')
 
-#df = pd.read_parquet('yellow_tripdata_2016-02.parquet')
-#df.to_csv('yellow_tripdata_2016-02.csv')
+df = pd.read_parquet('yellow_tripdata_2016-02.parquet')
+df.to_csv('yellow_tripdata_2016-02.csv')
 
-#df = pd.read_parquet('yellow_tripdata_2016-03.parquet')
-#df.to_csv('yellow_tripdata_2016-03.csv')
+df = pd.read_parquet('yellow_tripdata_2016-03.parquet')
+df.to_csv('yellow_tripdata_2016-03.csv')
 
-# #converter fhv_tripdat
-#df = pd.read_parquet('fhv_tripdata_2015-01.parquet')
-#df.to_csv('fhv_tripdata_2015-01.csv')
+#converter fhv_tripdat
+df = pd.read_parquet('fhv_tripdata_2015-01.parquet')
+df.to_csv('fhv_tripdata_2015-01.csv')
 
-#df = pd.read_parquet('fhv_tripdata_2016-01.parquet')
-#df.to_csv('fhv_tripdata_2016-01.csv')
+df = pd.read_parquet('fhv_tripdata_2016-01.parquet')
+df.to_csv('fhv_tripdata_2016-01.csv')
 
-#df = pd.read_parquet('fhv_tripdata_2016-02.parquet')
-#df.to_csv('fhv_tripdata_2016-02.csv')
+df = pd.read_parquet('fhv_tripdata_2016-02.parquet')
+df.to_csv('fhv_tripdata_2016-02.csv')
 
-#df = pd.read_parquet('fhv_tripdata_2016-03.parquet')
-#df.to_csv('fhv_tripdata_2016-03.csv')
+df = pd.read_parquet('fhv_tripdata_2016-03.parquet')
+df.to_csv('fhv_tripdata_2016-03.csv')
 
 
-# #converter green cabs
+#converter green cabs
 df = pd.read_parquet('green_tripdata_2015-01.parquet')
 df.to_csv('green_tripdata_2015-01.csv')
 
